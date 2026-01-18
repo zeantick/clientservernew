6c8387216334879f1fd07517ceb474ac908f6b12
zeantick <aivan2575@gmail.com>
2026-01-18 23:29:06 +0200

    "Fix Render setup"
    "Fix Render setup"

--- server.js
+++ server.js
@@ -93,5 +93,8 @@ app.get("/api/nearest_stop", async (req,res)=>{
 // ───────── Serve frontend ─────────
 app.use(express.static(path.join(__dirname,"public")));
 
-const PORT=3000;
-app.listen(PORT, ()=>{ console.log(`Server running at http://localhost:${PORT}`); });
+// Для Render
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
